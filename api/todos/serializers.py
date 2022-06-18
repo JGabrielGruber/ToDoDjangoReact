d96df07aa9301201869280798753372f1366f756
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2022-06-18 13:01:59 -0300

    Created API Todo serializer; Implemented API Todo serializer test;

---
+++ api/todos/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=True, max_length=200)
+    description = serializers.CharField(
+        style={'base_template': 'textarea.html'}
+    )
+    done = serializers.BooleanField(default=False)
+    created = serializers.DateTimeField(read_only=True)
+    edited = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        """
+        Create and return a new 'Todo' instance, given the validated data.
+        """
+        return Todo.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing 'Todo' instance,
+        given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get(
+            'description',
+            instance.description
+        )
+        instance.save()
+        return instance
--- api/todos/tests.py
+++ api/todos/tests.py
@@ -1,6 +1,10 @@
-from turtle import title
 from django.test import TestCase
+from io import BytesIO
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
 from .models import Todo
+from .serializers import TodoSerializer
 
 
 class TodosTestCase(TestCase):
@@ -9,8 +13,18 @@ class TodosTestCase(TestCase):
             title="Test task",
             description="This is a test task, please, ignore it."
         )
-    
+
     def test_todo_exists(self):
         """Check if Todo is correctly being created"""
         task = Todo.objects.get(title="Test task")
         self.assertGreaterEqual(task.edited, task.created)
+
+    def test_todo_serializer(self):
+        """Check if Todo is correctly being serialized"""
+        task = Todo.objects.get(title="Test task")
+        serializer = TodoSerializer(task)
+        content = JSONRenderer().render(serializer.data)
+        stream = BytesIO(content)
+        data = JSONParser().parse(stream)
+        serialized = TodoSerializer(data=data)
+        self.assertTrue(serialized.is_valid())
